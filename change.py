99b2419e21168ef73af25d456ad09b45f423a4a7
Mateo <dzmateozd@gmail.com>
2024-03-22 11:33:24 -0300

    Entrega TP2

--- change.py
+++ change.py
@@ -1,3 +1,10 @@
 def change():
     expense = 23.75
     money = 100
+    print(f"Ingresar gasto:\n{expense}")
+    print(f"Dinero recibido\n100\n\nVuelto\n")
+
+    total = money - expense
+    
+    print(f"Pesos:\n{int(total)}")
+    print(f"Centavos:\n{int((total - int(total)) * 100)}")
